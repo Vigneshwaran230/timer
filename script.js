36723fb15888e4f4e51d9631c7ccd24c4d736f62
Vigneshwaran230 <85312813+Vigneshwaran230@users.noreply.github.com>
2021-08-23 08:32:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+setTimeout(() => {
+    document.write(10);
+    setTimeout(() => {
+      document.write(9);
+      setTimeout(() => {
+        document.write(8);
+        setTimeout(() => {
+         document.write(7);
+          setTimeout(() => {
+            document.write(6);
+            setTimeout(() => {
+              document.write(5);
+              setTimeout(() => {
+                document.write(4);
+                setTimeout(() => {
+                  document.write(3);
+                  setTimeout(() => {
+                    document.write(2);
+                    setTimeout(() => {
+                     document.write(1);
+                      setTimeout(() => {
+                        document.write("Happy Independence Day");
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+  
+  
+  
